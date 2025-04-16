baff1f5ba07c07f8e529dd11ed3be38b6bb2e60d
bongx514 <bonganithwala514@live.com>
2025-04-16 20:36:36 +0200

    Creating footer

--- src/app/experience-page/experience-page.component.ts
+++ src/app/experience-page/experience-page.component.ts
@@ -14,7 +14,7 @@ export class ExperiencePageComponent implements AfterViewInit {
   @ViewChild('scrambleText') scrambleText!: ElementRef;
 
   pageTitle = 'Experience';
-  buttonCaption = 'See More';
+  buttonCaption = 'Learn More...';
   buttonLink = '/projects';
 
   phrases = [
@@ -26,9 +26,9 @@ export class ExperiencePageComponent implements AfterViewInit {
   characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()';
 
   cards = [
-    { title: 'Software 1', description: 'Developing web applications using Angular and Node.js.' },
-    { title: 'Software 2', description: 'Developing web applications using Angular and Node.js.' },
-    { title: 'Software 3', description: 'Developing web applications using Angular and Node.js.' }
+    { title: 'Frontend Developer', description: 'Developing ecommerce web application using magento 2 (adobe ecommerce) for various clients' },
+    { title: 'IT Intern', description: 'Developing and implementing a user-friendly and responsive Admin portal for Zarfinco Insurance using HTML5, CSS3, and JavaScript.' },
+    { title: 'Sales Rep', description: 'Greeting and welcoming customers as they enter the store.' }
   ];
 
   ngAfterViewInit(): void {
